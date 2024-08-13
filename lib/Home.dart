d498480ea13c4be2b427c159df6971335c07eecf
Maryiamreda <maryiamreda@gmail.com>
2024-08-14 01:29:56 +0300

    popular books row

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:ebook_app/app_colors.dart' as Appcolors;
 
@@ -9,6 +11,26 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late List popularBooks;
+  ReadData() async {
+    await DefaultAssetBundle /*Asset bundles are how Flutter apps access resources that are built into the app. */
+            .of(context)
+        .loadString /*This method loads the contents of the file located at "json/popularBooks" as a string*/ (
+            "json/popularBooks.json")
+        .then((s) {
+      setState(() {
+        popularBooks = json.decode(s);
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    ReadData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,11 +41,11 @@ class _HomeState extends State<Home> {
           children: [
             Container(
               margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
-              child: Row(
+              child: const Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   ImageIcon(
-                    AssetImage("img/menu.png"),
+                    AssetImage("assets/img/menu.png"),
                     size: 24,
                     color: Colors.black,
                   ),
@@ -39,6 +61,53 @@ class _HomeState extends State<Home> {
                 ],
               ),
             ),
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(left: 20, top: 20),
+                  child: const Text(
+                    "Popular Books",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 180,
+              child: Stack(
+                children: [
+                  Positioned(
+                      left: -20,
+                      right: 0,
+                      top: 0,
+                      child: Container(
+                        height: 180,
+                        child: PageView.builder(
+                            controller: PageController(
+                                // initialPage: 2,
+                                viewportFraction: 0.8),
+                            itemCount:
+                                popularBooks == null ? 0 : popularBooks.length,
+                            itemBuilder: (_, i) {
+                              return Container(
+                                height: 180,
+                                width: MediaQuery.of(context).size.width,
+                                margin: const EdgeInsets.only(right: 10),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15),
+                                    image: DecorationImage(
+                                        image:
+                                            AssetImage(popularBooks[i]["img"]),
+                                        fit: BoxFit.fill)),
+                              );
+                            }),
+                      ))
+                ],
+              ),
+            )
           ],
         ),
       )),
